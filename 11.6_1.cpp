13a911f6d34508a07f891e88ffae277c867d8a3d
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-11-16 09:00:07 +0300

    Ввод работает через раз не ясно почему.

--- 11.6_1.cpp
+++ 11.6_1.cpp
@@ -4,19 +4,8 @@
 
 
 
-std::string text;\
+std::string text;
 int ident;
-
-
-int main ()
-{
-    std::cout << "Input the text below: \n";
-    std::cin >> text;
-    std::cout << "Input the ident below : \n";
-    std::cin >> ident;
-    encrypt_caesar(text, ident);
-
-}
 
 
 std::string encrypt_caesar(std::string text, int ident){
@@ -24,11 +13,32 @@ std::string encrypt_caesar(std::string text, int ident){
     std::string ceasarText;
 
     for ( int i = 0; i != text.length(); i++){
+        std::string textSumm;
         if (text[i] == 32){
-            std::string textSumm;
-            textSumm = text[i + ident];
-            ceasarText += textSumm;
+            ceasarText += " ";
             }
+        else if ((text[i] >= 65 && text[i] <=90) || (text[i] >= 97 && text[i] <= 122)){
+            ceasarText += text[i + ident];
+            }
+        else {
+            std::cout << "There are another symbols!";
+            break;
+        }
     }
+    std::cout << " The code of Ceaser:\n" << ceasarText;
+    return 0;
+}
 
-} 
+
+    int main ()
+{
+    std::cout << "Input the text below: \n";
+    std::cin >> text;
+    std::cout << "Input the ident below : \n";
+    std::cin >> ident;
+
+    encrypt_caesar(text, ident);
+    return 0;
+
+}
+
