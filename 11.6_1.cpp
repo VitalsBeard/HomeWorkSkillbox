74c09fc447ca279a16b1f60883de6db8cfc4de1a
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-11-20 11:27:26 +0300

    First one is done but there are 1 mistake

--- 11.6_1.cpp
+++ 11.6_1.cpp
@@ -6,21 +6,27 @@
 std::string encrypt_caesar(std::string text, int ident){
 
     std::string ceasarText;
+    bool correct = true;
 
     for ( int i = 0; i != text.length(); i++){
-        std::string textSumm;
-        if (text[i] == 32){
-            ceasarText += " ";
-            }
-        else if ((text[i] >= 65 && text[i] <=90) || (text[i] >= 97 && text[i] <= 122)){
+       
+        if ((text[i] >= 65 && text[i] <=90) || (text[i] >= 97 && text[i] <= 122) || text[i] == 32)
+            {
+            if (text[i] > 91 ) i = 65;
+            if (text[i] > 122) i = 97;
             ceasarText += text[i + ident];
             }
         else {
-            std::cout << "There are another symbols!";
+            std::cout << "There are incorrect symbols!";
+            correct = false;
             break;
         }
+
     }
-    std::cout << " The code of Ceaser:\n" << ceasarText;
+    if (correct)
+    {
+    std::cout << "The code of Ceaser:\n" << ceasarText;
+    }
     return "";
 }
 
@@ -31,7 +37,7 @@ std::string encrypt_caesar(std::string text, int ident){
     std::string text;
 
     std::cout << "Input the text below: \n";
-    std::getline(text,text);
+    std::getline(std::cin,text);
     std::cout << "Input the ident below : \n";
     std::cin >> ident;
 
