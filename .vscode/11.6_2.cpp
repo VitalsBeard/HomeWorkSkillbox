c15493bb8698db9d28bfef91bc93220e25e28b76
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-11-28 10:15:37 +0300

    incorrect 2-nd part!

--- .vscode/11.6_2.cpp
+++ .vscode/11.6_2.cpp
@@ -4,62 +4,140 @@
 
 
 
-int chekMail( std::string adressName)
-{
-  int count = 0;
-  std::string correctMail;
- bool flag = true;
-
- for (int i = 0; i!= adressName.length(); i ++){
-    for ( int i = 0; i != '@' ; i++){
-    count++;
-
-    if( adressName [i] == '.' && adressName [i+1]== '.'){
-          flag = false;
-           break;
-    }else if ((adressName[i] >= 33 && adressName[i] <= 39) || (adressName[i] >= 42 && adressName[i] <= 57) || (adressName[i] >= 64 && adressName[i] <= 90) || 
-             (adressName[i] >= 97 && adressName[i] <= 126)){
-
-            correctMail += (char)adressName[i];
-            flag = true;
-            } 
+std::string checkEmailAdressFirstPart(std::string emailAdress){
+  bool flag = true;
+  std::string correctFirstPArt;
+  for (int i =0 ; i != '@'; i++){
+    if(emailAdress[i] == '.' && emailAdress[i +1] == '.' ){
+       flag = false;
+       break;
+    }else if (i>64){
+      flag = false;
+    }else if ((emailAdress[i] >= 33 && emailAdress[i] <= 39) || (emailAdress[i] >= 42 && emailAdress[i] <= 57) || (emailAdress[i] >= 64 && emailAdress[i] <= 90) || 
+             (emailAdress[i] >= 97 && emailAdress[i] <= 126)){
+        correctFirstPArt += (char)emailAdress[i];
+    }else{
+      flag = false;
+    }
   }
 
-
-    correctMail+='@';
-
-    for(int i = count ; i != adressName.length() ; i++){
-        if( (adressName [i] == '.' && adressName [i+1]== '.') || (adressName[i] = '@')){
-          flag = false;
-           break;
-        }else if ((adressName[i] >= 33 && adressName[i] <= 39) || (adressName[i] >= 42 && adressName[i] <= 57) || (adressName[i] >= 64 && adressName[i] <= 90) || 
-             (adressName[i] >= 97 && adressName[i] <= 126)){
-
-            correctMail += (char) adressName[i];
-            flag = true;
-            }else{
-
-                std::cout << "NO!";
-                break;
-            }
+  if (flag){
+    return "YES!";
+  }else{
+    std::cout << "Incorrect 1-st part!\n";
+    return "NO";
     }
 
+}
+
+std::string checkEmailAdressSecondPart(std::string emailAdress){
+ int count =0;
+ bool flag = true;
+ std::string correctSecondPart;
+
+ while (emailAdress[count] !='@'){
+  count++;
+ }
+
+ for (int i = count+1 ; i != emailAdress.length(); i++){
+    if (emailAdress[i] == '@'){
+      flag = false;
+      break;
+    }else if ((emailAdress[i] >= 33 && emailAdress[i] <= 39) || (emailAdress[i] >= 42 && emailAdress[i] <= 57) || (emailAdress[i] >= 64 && emailAdress[i] <= 90) || 
+             (emailAdress[i] >= 97 && emailAdress[i] <= 126)){
+      correctSecondPart += (char)emailAdress[i];
+       }
+ }
+
+if (flag){
+  return "Yes";
+}else{
+  std::cout << "Incorrect input in second part!\n";
+  return "NO";
   }
+}
+
+
+int main(){
+  std::string emailAdress;
+  std::cout << "Input the email adress below:\n";
+  std::cin >> emailAdress;
+  // checkEmailAdressFirstPart(emailAdress);
+  // checkEmailAdressSecondPart(emailAdress);
+  std::cout << checkEmailAdressFirstPart(emailAdress) + checkEmailAdressSecondPart(emailAdress);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int chekMail( std::string adressName)
+// {
+//   int count = 0;
+//   std::string correctMail;
+//  bool flag = true;
+
+//  for (int i = 0; i!= adressName.length(); i ++){
+//     for ( int i = 0; i != '@' ; i++){
+//     count++;
+
+//     if( adressName [i] == '.' && adressName [i+1]== '.'){
+//           flag = false;
+//            break;
+//     }else if ((adressName[i] >= 33 && adressName[i] <= 39) || (adressName[i] >= 42 && adressName[i] <= 57) || (adressName[i] >= 64 && adressName[i] <= 90) || 
+//              (adressName[i] >= 97 && adressName[i] <= 126)){
+
+//             correctMail += (char)adressName[i];
+//             flag = true;
+//             } 
+//   }
+
+
+//     correctMail+='@';
+
+//     for(int i = count ; i != adressName.length() ; i++){
+//         if( (adressName [i] == '.' && adressName [i+1]== '.') || (adressName[i] = '@')){
+//           flag = false;
+//            break;
+//         }else if ((adressName[i] >= 33 && adressName[i] <= 39) || (adressName[i] >= 42 && adressName[i] <= 57) || (adressName[i] >= 64 && adressName[i] <= 90) || 
+//              (adressName[i] >= 97 && adressName[i] <= 126)){
+
+//             correctMail += (char) adressName[i];
+//             flag = true;
+//             }else{
+
+//                 std::cout << "NO!";
+//                 break;
+//             }
+//     }
+
+//   }
   
-  if (flag){
-    std::cout << "YES!";
-  }
- return 0;
-}
+//   if (flag){
+//     std::cout << "YES!";
+//   }
+//  return 0;
+// }
 
 
-int main (){
-    std::string adressName;
+// int main (){
+//     std::string adressName;
 
-    std::cout << "Input the mail adress: \n";
-    std::cin >> adressName;
-    chekMail(adressName);
-}
+//     std::cout << "Input the mail adress: \n";
+//     std::cin >> adressName;
+//     chekMail(adressName);
+// }
 
 
 
