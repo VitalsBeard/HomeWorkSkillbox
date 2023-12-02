95238282eb5569460aa7545f8d880365a1dd62b0
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-12-02 11:25:10 +0300

    4 - ?

---
+++ .vscode/11.6.4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int gameResult(std::string gameInput){
+
+}
+
+
+
+
+int main()
+{
+    std::string gameInput;
+    std::cout << "let's play!\n";
+    gameResult(gameInput);
+    
+}
