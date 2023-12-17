93cf559a36d5de7716262928dca9c3bba32d9472
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-12-17 09:01:46 +0300

    1 - done , 2 - ques

---
+++ .vscode/Practice 12/12.2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+
+// void bubblesort(int* l, int* r) {
+// 	int sz = r - l;
+// 	if (sz <= 1) return;
+// 	bool b = true;
+// 	while (b) {
+// 		b = false;
+// 		for (int* i = l; i + 1 < r; i++) {
+// 			if (*i > *(i + 1)) {
+// 				swap(*i, *(i + 1));
+// 				b = true;
+// 			}
+// 		}
+// 		r--;
+// 	}
+// }
+
+
+
+
+
+
+
+int main()
+{
+    float numbers[15];
+    cout << "Please input the numbers: \n";
+    for (int i=0; i != 15; i++ )
+    {
+        cin >> numbers[i];
+    }
+
+    for (int i; i != 15; i++)
+    {
+        if(numbers[i] >= numbers[i+1]) swap(numbers[i] , numbers[i+1]);
+        else 
+        numbers[i] = numbers[i];
+    }
+    
+    cout << "Sorted numbers: \n" << numbers[15];
+}   
