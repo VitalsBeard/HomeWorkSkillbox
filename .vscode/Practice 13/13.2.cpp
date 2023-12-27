91c90daebf9085acb7e5cf7142f254110916a982
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2023-12-27 08:43:23 +0300

    2- complete

---
+++ .vscode/Practice 13/13.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std
+
+int main()
+{
+      vector<float> prices{2.5, 4.25, 3.0, 10.0};
+      vector<int> goods{1, 1, 0, 3};
+      float summAll=0;
+
+      for(int i = 0 ; i != goods.size(); i++)
+      {
+          summAll += prices(goods[i]);
+      }
+
+      cout << "Summ of the products is "<< summAll;
+}
