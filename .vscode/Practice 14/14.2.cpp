de2dafd2c71611b57dae342d1abc237538d67f27
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2024-01-17 11:53:26 +0300

    1

--- .vscode/Practice 14/14.2.cpp
+++ .vscode/Practice 14/14.2.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 
 
-char round ()
-{
-
-}
-
 
 int main()
 {
@@ -16,5 +11,11 @@ int main()
     bool places[3][3]= {{false,false,false},{false,false,false},{false,false,false}};
     bool check = false;
     char answer[3][3] = {{0,0,0},{0,0,0},{0,0,0}} ;
+
+    for (int i=0; i!=9; i++){
+        for (int j=0; j!=3; j++){
+            cout<< "Please input the correct ";
+        }
+    }
 
 } 
