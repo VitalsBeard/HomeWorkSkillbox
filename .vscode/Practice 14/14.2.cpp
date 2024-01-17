ab173c6058a2c6abd8255040959b9c55eaf2b775
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2024-01-17 10:25:50 +0300

    1 -5050 2 -started

---
+++ .vscode/Practice 14/14.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+char round ()
+{
+
+}
+
+
+int main()
+{
+    char playerChoice;
+    bool places[3][3]= {{false,false,false},{false,false,false},{false,false,false}};
+    bool check = false;
+    char answer[3][3] = {{0,0,0},{0,0,0},{0,0,0}} ;
+
+} 
