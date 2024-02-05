1cdaab0ef8278f22cc68ce78780c7f72bb55e7b2
Vitaliy Borodulin <vitalsbeard@yandex.ru>
2024-02-05 20:11:27 +0300

    complete

--- .vscode/Practice_15/15.4.cpp
+++ .vscode/Practice_15/15.4.cpp
@@ -12,6 +12,7 @@ int main()
 {
     vector <int> vec = {-100,-50, -5, 1, 10, 15};
     vector <int> correct_vec;
+    correct_vec.resize(vec.size());
     int n;
 
     for (int i = 0; i != vec.size(); i ++){
@@ -19,12 +20,12 @@ int main()
     }
     for(int j = 0; j!=correct_vec.size(); j++){
         for(int k =0; k!= correct_vec.size(); k++){
-            if(correct_vec[j]>=correct_vec[k]) swap (correct_vec[j],correct_vec[k]);
+            if(correct_vec[j]>=correct_vec[k]) swap (correct_vec[k],correct_vec[j]);
         }
     }
 
     for(int i =0; i!=vec.size();i++ ){
-        cout<< correct_vec[i];
+        cout<< correct_vec[i]<< " ";
     }
 
     cin >> n;
